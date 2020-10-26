90bef409ee094bfd21510f8bb2d7b811874ce01d
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-10-25 21:13:20 -0700

    Move routes to route folder

--- server.js
+++ server.js
@@ -1,49 +1,18 @@
 const express = require('express');
 const dotenv = require('dotenv');
+// Routes
+const nonprofits = require('./routes/nonprofits');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
-// CRUD routes for non-profits
-app.get('/api/v1/nonprofits', (req, res) => {
-	res.status(200).json({
-		success: true,
-		data: `Get all non-profits`
-	})
-})
-
-app.get('/api/v1/nonprofits/:id', (req, res) => {
-	res.status(200).json({
-		success: true,
-		data: `Get a single non-profit. Id: ${req.params.id}`
-		})
-})
-
-app.post('/api/v1/nonprofits', (req, res) => {
-	res.status(200).json({
-		success: true,
-		data: 'create a new non-profit'
-	})
-})
-
-app.put('/api/v1/nonprofits/:id', (req, res) => {
-	res.status(200).json({
-		success: true,
-		data: `Udpate a non-profit. Id: ${req.params.id}`
-	})
-})
-
-app.delete('/api/v1/nonprofits/:id', (req, res) => {
-	res.status(200).json({
-		success: true,
-		data: `Delete a non-profit. Id: ${req.params.id}`
-	})
-})
-
+// Mount Routers
+app.use('/api/v1/nonprofits', nonprofits);
 
 const PORT = process.env.PORT | 5000;
+
 app.listen(
 	PORT,
 	console.log(`
