4907f2c5b8e48c7a3bf988a938785863f7f902e2
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-10-29 21:45:02 -0700

    Refactor spacing

--- server.js
+++ server.js
@@ -1,34 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-const colors = require('colors');
-const connectDB = require('./config/database');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const colors = require("colors");
+const connectDB = require("./config/database");
 
 // Load env variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 // Connect to database
 connectDB();
 
 // Routes
-const nonprofits = require('./routes/nonprofits');
+const nonprofits = require("./routes/nonprofits");
 const app = express();
 app.use(express.json());
 
-
 // @desc	mount logger
-if (process.env.NODE_ENV === 'development') {
-	app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Mount Routers
-app.use('/api/v1/nonprofits', nonprofits);
+app.use("/api/v1/nonprofits", nonprofits);
 
 const PORT = process.env.PORT | 5000;
 
 app.listen(
-	PORT,
-	console.log(`
+  PORT,
+  console.log(
+    `
     Listening in ${process.env.NODE_ENV} on port ${PORT}
-`.yellow.bold),
+`.yellow.bold
+  )
 );
