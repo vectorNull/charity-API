11039a299e136565eb9644c95091cc0da2b9dd0d
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-10-30 23:07:52 -0700

    Refactor to use asyncHandler

--- controllers/nonprofits.js
+++ controllers/nonprofits.js
@@ -1,98 +1,92 @@
 const Nonprofit = require("../models/Nonprofit");
+const ErrorResponse = require("../utils/ErrorResponse");
+const asyncHandler = require("../middleware/async");
 
 // @desc    Get all nonprofits
 // @route   GET /api/v1/nonprofits
 // @access  Public
-exports.getNonprofits = async (req, res, next) => {
-	try {
-		const nonprofits = await Nonprofit.find();
-		res.status(200).json({
-			success: true,
-			count: nonprofits.length,
-			data: nonprofits,
-		});
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+exports.getNonprofits = asyncHandler(async (req, res, next) => {
+    const nonprofits = await Nonprofit.find();
+    res.status(200).json({
+        success: true,
+        count: nonprofits.length,
+        data: nonprofits,
+    });
+});
 
 // @desc    Get a single nonprofit
 // @route   GET /api/v1/nonprofits/:id
 // @access  Private
-exports.getNonprofit = async (req, res, next) => {
-	try {
-		const nonprofit = await Nonprofit.findById(req.params.id);
-		res.status(200).json({
-			success: true,
-			data: nonprofit,
-		});
-		if (!nonprofit) {
-			// checks for null and by extension checks for properly formatted id
-			return res.status(400).json({ success: false });
-		}
-	} catch (error) {
-		res.status(400).json({ success: false });
-	}
-};
+exports.getNonprofit = asyncHandler(async (req, res, next) => {
+    const nonprofit = await Nonprofit.findById(req.params.id);
+    res.status(200).json({
+        success: true,
+        data: nonprofit,
+    });
+    if (!nonprofit) {
+        // checks for null and by extension checks for properly formatted id
+        return next(
+            new ErrorResponse(
+                `Resource not found with id of ${req.params.id}`,
+                400
+            )
+        );
+    }
+});
 
-// @desc    create a single nonprofits
+// @desc    create a single nonprofit
 // @route   POST /api/v1/nonprofits
 // @access  Private
-exports.createNonprofit = async (req, res, next) => {
-	console.log(req.body);
-	try {
-		const nonprofit = await Nonprofit.create(req.body);
-		res.status(201).json({
-			success: true,
-			data: nonprofit,
-		});
-	} catch (err) {
-		res.status(400).json({ success: false });
-		console.log(err);
-	}
-};
+exports.createNonprofit = asyncHandler(async (req, res, next) => {
+    const nonprofit = await Nonprofit.create(req.body);
+    res.status(201).json({
+        success: true,
+        data: nonprofit,
+    });
+});
+
 // @desc    Update a nonprofit
 // @route   PUT /api/v1/nonprofits
 // @access  Private
-exports.updateNonprofit = async (req, res, next) => {
-	try {
-		const nonprofit = await Nonprofit.findByIdAndUpdate(
-			req.params.id,
-			req.body,
-			{
-				new: true,
-				runValidators: true,
-			}
-		);
-		if (!nonprofit) {
-			return res.status(400).json({ success: false });
-		}
-		res.status(200).json({
-			success: true,
-			data: nonprofit,
-		});
-	} catch (error) {
-		res.status(400).json({ success: false })
-	}
-	
-};
+exports.updateNonprofit = asyncHandler(async (req, res, next) => {
+    const nonprofit = await Nonprofit.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+            new: true,
+            runValidators: true,
+        }
+    );
+    if (!nonprofit) {
+        return next(
+            new ErrorResponse(
+                `Resource not found with id of ${req.params.id}`,
+                400
+            )
+        );
+    }
+    res.status(200).json({
+        success: true,
+        data: nonprofit,
+    });
+});
 
 // @desc    Delete a nonprofit
 // @route   DELETE /api/v1/nonprofits
 // @access  Private
-exports.deleteNonprofit = async (req, res, next) => {
-	try {
-		const nonprofit = await Nonprofit.findByIdAndDelete(
-			req.params.id,
-		);
-		if (!nonprofit) {
-			return res.status(400).json({ success: false });
-		}
-		res.status(200).json({
-			success: true,
-			data: {}
-		});
-	} catch (error) {
-		res.status(400).json({ success: false })
-	}
-};
+exports.deleteNonprofit = asyncHandler(async (req, res, next) => {
+        const nonprofit = await Nonprofit.findByIdAndDelete(req.params.id);
+        if (!nonprofit) {
+            return next(
+                new ErrorResponse(
+                    `Resource not found with id of ${req.params.id}`,
+                    400
+                )
+            );
+        }
+        res.status(200).json({
+            success: true,
+            data: {},
+        });
+   
+});
