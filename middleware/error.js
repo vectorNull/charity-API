14f2184ef8fff7e4b144cf0c63c09d352835ca27
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-10-30 23:08:25 -0700

    Create errorHandler function

---
+++ middleware/error.js
@@ -0,0 +1,31 @@
+const ErrorResponse = require("../utils/ErrorResponse");
+
+const errorHandler = (err, req, res, next) => {
+    let error = { ...err };
+    error.message = err.message;
+    
+    // Mongoose bad ObjectId
+    if (err.name === "CastError") {
+        const message = `Resource not found with id of ${err.value}`;
+        error = new ErrorResponse(message, 404);
+    }
+
+    // Mongoose dublicate key based on err.code
+    if (err.code == 11000) {
+        const message = "Duplicate field value entered";
+        error = new ErrorResponse(message, 400);
+    }
+
+    // Mongoose validation errors
+    if (err.name == "ValidationError") {
+        const message = Object.values(err.errors).map((val) => val.message);
+        console.log(Object.values(err.errors));
+        error = new ErrorResponse(message, 400);
+    }
+    res.status(error.statusCode || 500).json({
+        success: false,
+        error: error.message || "Server Error",
+    });
+};
+
+module.exports = errorHandler;
