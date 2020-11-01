83006ec2a2a3fa992c397f8d2b65c0385af29220
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 00:03:55 -0700

    Geo route

--- routes/nonprofits.js
+++ routes/nonprofits.js
@@ -1,25 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Deconstruct controllers
 const {
-	getNonprofits,
-	getNonprofit,
-	createNonprofit,
-	updateNonprofit,
-	deleteNonprofit,
-} = require('../controllers/nonprofits');
+    getNonprofits,
+    getNonprofit,
+    createNonprofit,
+    updateNonprofit,
+    deleteNonprofit,
+    getNonprofitsInRadius,
+} = require("../controllers/nonprofits");
+
+// Geo route
+router.route("/radius/:zipcode/:distance").get(getNonprofitsInRadius);
 
 // Root routes
-router.route('/')
-    .get(getNonprofits)
-    .post(createNonprofit);
+router.route("/").get(getNonprofits).post(createNonprofit);
 
 // Routes requiring id
 router
-	.route('/:id')
-	.get(getNonprofit)
-	.put(updateNonprofit)
-	.delete(deleteNonprofit);
+    .route("/:id")
+    .get(getNonprofit)
+    .put(updateNonprofit)
+    .delete(deleteNonprofit);
 
 module.exports = router;
