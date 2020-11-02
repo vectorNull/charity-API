5650493bcf74e50247a1dd1e1e44ab68b6ab07fe
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 23:54:38 -0800

    Refactor to use advancedResults middleware

--- controllers/nonprofits.js
+++ controllers/nonprofits.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const geocoder = require("../utils/geocoder");
 const Nonprofit = require("../models/Nonprofit");
 const ErrorResponse = require("../utils/ErrorResponse");
@@ -7,71 +8,7 @@ const asyncHandler = require("../middleware/async");
 // @route   GET /api/v1/nonprofits
 // @access  Public
 exports.getNonprofits = asyncHandler(async (req, res, next) => {
-    let query;
-    // copy req.query
-    const reqQuery = { ...req.query };
-    // Fields to exclude
-    const removeFields = ["select", "sort", "page", "limit"];
-    // Loop over removeFields and delete them from reqQuery
-    removeFields.forEach((param) => delete reqQuery[param]);
-
-    // Create query string
-    let queryStr = JSON.stringify(reqQuery);
-    // Create operators
-    queryStr = queryStr.replace(
-        /\b(gt|gte|lt|lte|in)\b/g,
-        (match) => `$${match}`
-    );
-    // Finding resource
-    query = Nonprofit.find(JSON.parse(queryStr)).populate('programs');
-    // Select Fields
-    if (req.query.select) {
-        const fields = req.query.select.split(",").join(" ");
-        query = query.select(fields);
-    }
-
-    // Sort
-    if (req.query.sort) {
-        const sortBy = req.query.sort.split(",").join(" ");
-        query = query.sort(sortBy);
-    } else {
-        query = query.sort("-createdAt");
-    }
-
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 25;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const total = await Nonprofit.countDocuments();
-
-    query = query.skip(startIndex).limit(limit);
-
-    // Executing query
-    const nonprofits = await query;
-
-    // Pagination result
-    const pagination = {};
-    if (startIndex > 0) {
-        pagination.prev = {
-            page: page - 1,
-            limit,
-        };
-    }
-
-    if (endIndex < total) {
-        pagination.next = {
-            page: page + 1,
-            limit,
-        };
-    }
-
-    res.status(200).json({
-        success: true,
-        count: nonprofits.length,
-        pagination,
-        data: nonprofits,
-    });
+    res.status(200).json(res.advancedResults);
 });
 
 // @desc    Get a single nonprofit
@@ -145,7 +82,7 @@ exports.deleteNonprofit = asyncHandler(async (req, res, next) => {
         );
     }
     nonprofit.remove();
-    
+
     res.status(200).json({
         success: true,
         data: {},
@@ -174,5 +111,54 @@ exports.getNonprofitsInRadius = asyncHandler(async (req, res, next) => {
         success: true,
         count: nonprofits.length,
         data: nonprofits,
+    });
+});
+
+// @desc    Upload photo for nonprofit
+// @route   PUT /api/v1/nonprofits/:id/photo
+// @access  Private
+exports.nonprofitPhotoUpload = asyncHandler(async (req, res, next) => {
+    const nonprofit = await Nonprofit.findById(req.params.id);
+    if (!nonprofit) {
+        return next(
+            new ErrorResponse(
+                `Resource not found with id of ${req.params.id}`,
+                400
+            )
+        );
+    }
+    if (!req.files) {
+        return next(new ErrorResponse(`Please upload a file`, 400));
+    }
+
+    const file = req.files.file;
+    console.log(file);
+    // Validation to make sure file is photo
+    if (!file.mimetype.startsWith("image")) {
+        return next(new ErrorResponse(`Please upload an image file`, 400));
+    }
+
+    // check file size
+    if (file.size > process.env.MAX_FILE_UPLOAD) {
+        return next(
+            new ErrorResponse(
+                `Please upload an image file less than ${process.env.MAX_FILE_UPLOAD}`,
+                400
+            )
+        );
+    }
+
+    // Create custom file name to prevent overwrite
+    file.name = `photo_${nonprofit._id}${path.parse(file.name).ext}`;
+    file.mv(`${process.env.FILE_UPLOAD_PATH}/${file.name}`, async (err) => {
+        if (err) {
+            console.error(err);
+            return next(new ErrorResponse(`Problem with file upload`, 500));
+        }
+        await Nonprofit.findByIdAndUpdate(req.params.id, { photo: file.name });
+        res.status(200).json({
+            success: true,
+            data: file.name
+        })
     });
 });
