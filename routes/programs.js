ea26a41b3ffaddd87445aba319dfa032e9dbc3df
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 21:59:08 -0800

    Get, add, and update routes

--- routes/programs.js
+++ routes/programs.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 // Deconstruct controllers
 const {
-    getPrograms
+    getPrograms,
+    getProgram,
+    addProgram,
+    updateProgram,
 } = require("../controllers/programs");
 
 const router = express.Router({ mergeParams: true });
 
-
-router.route('/').get(getPrograms);
+router.route("/").get(getPrograms).post(addProgram);
+router.route("/:id").get(getProgram).put(updateProgram);
 
 module.exports = router;
