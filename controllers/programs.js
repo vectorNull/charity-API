0d945dc6568de772bcb72db2d7374e6823d8c190
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 21:58:16 -0800

    Get, update, and add single program

--- controllers/programs.js
+++ controllers/programs.js
@@ -1,4 +1,5 @@
 const Program = require("../models/Program");
+const Nonprofit = require("../models/Nonprofit");
 const ErrorResponse = require("../utils/ErrorResponse");
 const asyncHandler = require("../middleware/async");
 
@@ -8,19 +9,94 @@ const asyncHandler = require("../middleware/async");
 // @access  Public
 exports.getPrograms = asyncHandler(async (req, res, next) => {
     let query;
-    if(req.params.nonprofitId) {
+    if (req.params.nonprofitId) {
         query = Program.find({ nonprofitId: req.params.nonprofitId });
     } else {
         query = Program.find().populate({
-            path: 'nonprofitId',
-            select: 'name description'
+            path: "nonprofitId",
+            select: "name description",
         });
     }
     const programs = await query;
     res.status(200).json({
         success: true,
         count: programs.length,
-        data: programs
-    })
-})
+        data: programs,
+    });
+});
 
+// @desc    Get a single program
+// @route   GET /api/v1/courses/:id
+// @access  Public
+exports.getProgram = asyncHandler(async (req, res, next) => {
+    const program = await Program.findById(req.params.id).populate({
+        path: "nonprofitId",
+        select: "name description",
+    });
+
+    if (!program) {
+        return next(
+            new ErrorResponse(`No program with the id of ${req.params.id}`),
+            404
+        );
+    }
+
+    res.status(200).json({
+        success: true,
+        data: program,
+    });
+});
+
+// @desc    Add a single program
+// @route   POST /api/v1/nonprofits/:nonprofitId/courses
+// @access  Private
+exports.addProgram = asyncHandler(async (req, res, next) => {
+    
+    req.body.nonprofitId = req.params.nonprofitId;
+    // console.log(req.body)
+    // console.log(req.params.nonprofitId)
+    const nonprofit = await Nonprofit.findById(req.params.nonprofitId);
+    console.log(nonprofit)
+    if (!nonprofit) {
+        return next(
+            new ErrorResponse(
+                `No nonprofit with the id of ${req.params.nonprofitId}`
+            ),
+            404
+        );
+    }
+
+    const program = await Program.create(req.body);
+    //console.log(program)
+    res.status(200).json({
+        success: true,
+        data: program,
+    });
+});
+
+// @desc    Update a single program
+// @route   PUT /api/v1/programs/:id
+// @access  Private
+exports.updateProgram = asyncHandler(async (req, res, next) => {
+    
+    let program = await Program.findById(req.params.id);
+    console.log(program)
+    if (!program) {
+        return next(
+            new ErrorResponse(
+                `No program with the id of ${req.params.id}`
+            ),
+            404
+        );
+    }
+    program = await Program.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+
+    //console.log(program)
+    res.status(200).json({
+        success: true,
+        data: program,
+    });
+});
