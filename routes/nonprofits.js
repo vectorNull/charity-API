dfc7eabf2bc9583fec0ee3781dfb9f74d012aa49
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 23:56:00 -0800

    Set up advancedResults middleware

--- routes/nonprofits.js
+++ routes/nonprofits.js
@@ -9,19 +9,29 @@ const {
     updateNonprofit,
     deleteNonprofit,
     getNonprofitsInRadius,
+    nonprofitPhotoUpload,
 } = require("../controllers/nonprofits");
 
+const Nonprofit = require("../models/Nonprofit");
+const advancedResults = require("../middleware/advancedResults");
+
 // Include other resource routers
-const programRouter = require('./programs');
+const programRouter = require("./programs");
 
 // re-route into other resource routers
-router.use('/:nonprofitId/programs', programRouter)
+router.use("/:nonprofitId/programs", programRouter);
 
 // Geo route
 router.route("/radius/:zipcode/:distance").get(getNonprofitsInRadius);
 
+// Upload photo route
+router.route("/:id/photo").put(nonprofitPhotoUpload);
+
 // Root routes
-router.route("/").get(getNonprofits).post(createNonprofit);
+router
+    .route("/")
+    .get(advancedResults(Nonprofit, "programs"), getNonprofits)
+    .post(createNonprofit);
 
 // Routes requiring id
 router
