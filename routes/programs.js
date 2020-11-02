a43a584e2d058442a39efc562f660dcde53f4799
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 23:56:23 -0800

    Refactor for advancedResults middleware

--- routes/programs.js
+++ routes/programs.js
@@ -6,12 +6,24 @@ const {
     getProgram,
     addProgram,
     updateProgram,
-    deleteProgram
+    deleteProgram,
 } = require("../controllers/programs");
+
+const Program = require("../models/Program");
+const advancedResults = require("../middleware/advancedResults");
 
 const router = express.Router({ mergeParams: true });
 
-router.route("/").get(getPrograms).post(addProgram);
+router
+    .route("/")
+    .get(
+        advancedResults(Program, {
+            path: "nonprofitId",
+            select: "name description",
+        }),
+        getPrograms
+    )
+    .post(addProgram);
 router.route("/:id").get(getProgram).put(updateProgram).delete(deleteProgram);
 
 module.exports = router;
