4bfe669da11143600a30548991fef7fae08c6ad7
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-01 20:21:33 -0800

    Cascade detele programs with nonprofits

--- models/Nonprofit.js
+++ models/Nonprofit.js
@@ -2,98 +2,104 @@ const mongoose = require("mongoose");
 const slugify = require("slugify");
 const geocoder = require("../utils/geocoder");
 
-const NonprofitSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    slug: String,
-    email: {
-        type: String,
-        required: true,
-        match: [
-            /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-            "Please add a valid email",
-        ],
-    },
-    website: {
-        type: String,
-        match: [
-            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-            "Please use a valid URL with HTTP or HTTPS",
-        ],
-    },
-    guideStarAddress: {
-        type: String,
-        match: [
-            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
-            "Please use a valid URL with HTTP or HTTPS",
-        ],
-    },
-    phone: {
-        type: String,
-        maxlength: [20, "Phone number can not be longer than 20 characters"],
-    },
-    ein: {
-        type: String,
-        required: [true, "Please enter a valid EIN number"],
-    },
-    president: {
-        type: String,
-        maxlength: 200,
-    },
-    address: {
-        type: String,
-        required: true,
-    },
-    location: {
-        // GeoJSON Point
-        type: {
+const NonprofitSchema = new mongoose.Schema(
+    {
+        name: {
             type: String,
-            enum: ["Point"],
+            required: true,
         },
-        coordinates: {
-            type: [Number],
-            index: "2dsphere",
+        slug: String,
+        email: {
+            type: String,
+            required: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please add a valid email",
+            ],
         },
-        formattedAddress: String,
-        street: String,
-        city: String,
-        state: String,
-        zipcode: String,
-        country: String,
+        website: {
+            type: String,
+            match: [
+                /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+                "Please use a valid URL with HTTP or HTTPS",
+            ],
+        },
+        guideStarAddress: {
+            type: String,
+            match: [
+                /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
+                "Please use a valid URL with HTTP or HTTPS",
+            ],
+        },
+        phone: {
+            type: String,
+            maxlength: [
+                20,
+                "Phone number can not be longer than 20 characters",
+            ],
+        },
+        ein: {
+            type: String,
+            required: [true, "Please enter a valid EIN number"],
+        },
+        president: {
+            type: String,
+            maxlength: 200,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        location: {
+            // GeoJSON Point
+            type: {
+                type: String,
+                enum: ["Point"],
+            },
+            coordinates: {
+                type: [Number],
+                index: "2dsphere",
+            },
+            formattedAddress: String,
+            street: String,
+            city: String,
+            state: String,
+            zipcode: String,
+            country: String,
+        },
+        photo: {
+            type: String,
+            default: "no-photo.jpg",
+        },
+        description: {
+            type: String,
+            //required: true,
+        },
+        nteeCodes: {
+            type: Array,
+            nteeCode: {
+                type: Object,
+            },
+        },
+        verified: {
+            type: Boolean,
+            default: false,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
+        user: {
+            type: mongoose.Schema.ObjectId,
+            ref: "User",
+            //required: true,
+        },
     },
-    photo: {
-        type: String,
-        default: "no-photo.jpg",
-    },
-    description: {
-        type: String,
-        //required: true,
-    },
-    nteeCodes: {
-		type: Array,
-		nteeCode: {
-			type: Object
-		}
-    },
-    verified: {
-        type: Boolean,
-        default: false,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now
-    }, 
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: "User",
-        //required: true,
-    },
-}, {
-    toJSON: { virtuals: true},
-    toObject: { virtuals: true }
-});
+    {
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
+    }
+);
 
 // Create nonprofit slug from the name
 NonprofitSchema.pre("save", function (next) {
@@ -120,12 +126,18 @@ NonprofitSchema.pre("save", async function (next) {
     next();
 });
 
+// Cascade delete programs when deleting nonprofit
+NonprofitSchema.pre("remove", async function (next) {
+    await this.model("Program").deleteMany({ nonprofitId: this._id });
+    next();
+});
+
 // Reverse populate with virtuals
-NonprofitSchema.virtual('programs', {
-    ref: 'Program',
-    localField: '_id',
-    foreignField: 'nonprofitId',
-    justOne: false
-})
+NonprofitSchema.virtual("programs", {
+    ref: "Program",
+    localField: "_id",
+    foreignField: "nonprofitId",
+    justOne: false,
+});
 
 module.exports = mongoose.model("Nonprofit", NonprofitSchema);
