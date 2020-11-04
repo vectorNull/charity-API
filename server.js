2793c172ebea3600ea139a8b4024c8ceb70cddf9
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-04 01:16:33 -0800

    Use security modules

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const cookieParser = require('cookie-parser');
 const connectDB = require('./config/database');
 const errorHandler = require('./middleware/error');
 
+// Security
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors')
+
 const app = express();
 app.use(express.json());
 
@@ -23,6 +31,29 @@ connectDB();
 // File uploading
 app.use(fileupload());
 
+// Saniitize data
+app.use(mongoSanitize());
+
+// Set security headers
+app.use(helmet());
+
+// xss-clean
+app.use(xss());
+
+// rate limiting
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000,    // 10 mins
+    max: 100
+})
+
+app.use(limiter);
+
+// Prevent HTTP param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors())
+
 // set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -30,11 +61,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 const nonprofits = require('./routes/nonprofits');
 const programs = require('./routes/programs');
 const auth = require('./routes/auth');
+const users = require('./routes/users');
 
 // Mount Routers
 app.use('/api/v1/nonprofits', nonprofits);
 app.use('/api/v1/programs', programs);
 app.use('/api/v1/auth', auth);
+app.use('/api/v1/users', users);
 
 // Mount error handler
 app.use(errorHandler);
