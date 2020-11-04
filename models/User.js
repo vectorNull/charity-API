dc5b3d6e1bb744177fcffcff21013613e78e6df1
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-04 01:18:01 -0800

    Update User model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,8 @@
+const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const randomize = require('randomatic');
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -24,12 +26,23 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please add a password'],
-        minlength: 8,
+        minlength: 6,
         select: false,
     },
     resetPasswordToken: String,
     resetPasswordExpire: Date,
-    createAt: {
+    confirmEmailToken: String,
+    isEmailConfirmed: {
+        type: Boolean,
+        default: false,
+    },
+    twoFactorCode: String,
+    twoFactorCodeExpire: Date,
+    twoFactorEnable: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
         type: Date,
         default: Date.now,
     },
@@ -37,6 +50,10 @@ const UserSchema = new mongoose.Schema({
 
 // Encrypt password using bcrypt
 UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next();
+    }
+
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
 });
@@ -48,9 +65,41 @@ UserSchema.methods.getSignedJwtToken = function () {
     });
 };
 
-// Match user entered passwd to hashed password in database
+// Match user entered password to hashed password in database
 UserSchema.methods.matchPassword = async function (enteredPassword) {
-    return await bcrypt.compare(enteredPassword, this.password)
-} 
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
+// Generate and hash password token
+UserSchema.methods.getResetPasswordToken = function () {
+    // Generate token
+    const resetToken = crypto.randomBytes(20).toString('hex');
+
+    // Hash token and set to resetPasswordToken field
+    this.resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    // Set expire
+    this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+    return resetToken;
+};
+
+// Generate email confirm token
+UserSchema.methods.generateEmailConfirmToken = function (next) {
+    // email confirmation token
+    const confirmationToken = crypto.randomBytes(20).toString('hex');
+
+    this.confirmEmailToken = crypto
+        .createHash('sha256')
+        .update(confirmationToken)
+        .digest('hex');
+
+    const confirmTokenExtend = crypto.randomBytes(100).toString('hex');
+    const confirmTokenCombined = `${confirmationToken}.${confirmTokenExtend}`;
+    return confirmTokenCombined;
+};
 
 module.exports = mongoose.model('User', UserSchema);
