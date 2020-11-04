c4de3fe687fa7415a2fc347f033a8f800371fb11
Jose E. Rodriguez <jose.rodriguez76@protonmail.com>
2020-11-04 01:18:20 -0800

    Add password functionallity

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,27 @@
-const express = require('express')
+const express = require('express');
 
-const { register, login, getMe } = require('../controllers/auth')
+const {
+    register,
+    login,
+    getMe,
+    forgotPassword,
+    resetPassword,
+    updateDetails,
+    updatePassword,
+    logout
+} = require('../controllers/auth');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth')
+const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/me' ,protect,  getMe);
+router.get('/logout', logout);
+router.get('/me', protect, getMe);
+router.put('/updatedetails', protect, updateDetails)
+router.post('/forgotpassword', forgotPassword);
+router.put('/updatepassword', protect, updatePassword)
+router.put('/resetpassword/:resettoken', resetPassword);
 
 module.exports = router;
